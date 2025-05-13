bf94a88dfe1e355251edc07a9841cd2b7ccf7c7b
ChanceCtrl <>
2025-05-12 20:46:57 -0400

    Something working

--- include/main.hpp
+++ include/main.hpp
@@ -9,16 +9,21 @@
 #include "vectornav.hpp"
 
 // Vectornav setup
-vNav nd(Serial8);
+vNav nd(Serial2);
 
 // User input setup
 bool button1() { return bool(digitalRead(SW3)); }
 bool button2() { return bool(digitalRead(SW4)); }
 
-polButton a_button(150, &button1, true);
-polButton b_button(150, &button1, true);
+polButton a_button(300, &button1, true);
+polButton b_button(300, &button2, true);
 
 // User output setup
-Adafruit_NeoPixel pixels(NEOPIXEL_COUNT, NEOPIXEL_DIN, NEO_GRB + NEO_KHZ800);
+// Adafruit_NeoPixel pixels(NEOPIXEL_COUNT, NEOPIXEL_DIN, NEO_GRB + NEO_KHZ800);
 
 // File saving
+// class Logger track(teensy_microsd_numbered_log);
+// class Logger points(teensy_microsd_numbered_log);
+Metro flush_timer(500, true);
+File track;
+File points;
--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,87 @@
 #include "main.hpp"
 
 void setup() {
+  Serial.begin(9600);
+  Serial.println("init");
+
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  pinMode(SW3, INPUT_PULLUP);
+  pinMode(SW4, INPUT_PULLUP);
+
   nd.init(Logger);
-  pixels.begin();
+
+  // Wait for SD stuffs
+  delay(500);
+  if (!SD.begin(BUILTIN_SDCARD)) { // Begin Arduino SD API
+    Serial.println("SD card failed or not present");
+  }
+  delay(500);
+
+  // TODO: Make this not look braindead
+  char filename[] = "GPS_0000.CSV";
+  for (uint16_t i = 0; i < 10000; i++) {
+    filename[4] = i / 1000 + '0';
+    filename[5] = i / 100 % 10 + '0';
+    filename[6] = i / 10 % 10 + '0';
+    filename[7] = i % 10 + '0';
+    if (!SD.exists(filename)) { // Create and open file
+      track = SD.open(filename, (uint8_t)O_WRITE | (uint8_t)O_CREAT);
+      break;
+    }
+    if (i == 9999) { // Print if it cant
+      Serial.println("Ran out of names, clear SD");
+    }
+  }
+
+  char filename2[] = "PPS_0000.CSV";
+  for (uint16_t i = 0; i < 10000; i++) {
+    filename2[4] = i / 1000 + '0';
+    filename2[5] = i / 100 % 10 + '0';
+    filename2[6] = i / 10 % 10 + '0';
+    filename2[7] = i % 10 + '0';
+    if (!SD.exists(filename2)) { // Create and open file
+      points = SD.open(filename2, (uint8_t)O_WRITE | (uint8_t)O_CREAT);
+      break;
+    }
+    if (i == 9999) { // Print if it cant
+      Serial.println("Ran out of names, clear SD");
+    }
+  }
+
+  // Print guide at top of CSV
+  track.println("latitude, longitude");
+  track.flush();
+
+  // Print another guide
+  points.println("latitude, longitude");
+  points.flush();
 }
 
 void loop() {
-  if (Serial8.available() > 4) {
+  digitalToggle(LED_BUILTIN);
+
+  a_button.update(millis());
+
+  // Get data
+  if (Serial2.available() > 4) {
     if (nd.check_sync_byte()) {
-      // Get data
       nd.read_data();
 
-      // Yeet data
-      if (position_fix_send.check()) {
-        write_to_SD(vectornav_time, 3);
-        write_to_SD(vectornav_position, 3);
-      }
-
-      if (imu_data_send.check()) {
-        write_to_SD(vectornav_attitude, 3);
-        write_to_SD(vectornav_gyro, 3);
-        write_to_SD(vectornav_velocity, 3);
-        write_to_SD(vectornav_accel, 3);
-      }
+      track.print(String(nd.position.latitude) + ",");
+      track.println(String(nd.position.longitude));
     }
+  }
+
+  if (a_button.state == true && a_button.held == false) {
+    Serial.println("logging point");
+
+    points.print(String(nd.position.latitude) + ",");
+    points.println(String(nd.position.longitude));
+  }
+
+  if (flush_timer.check()) {
+    track.flush();
+    points.flush();
   }
 }
